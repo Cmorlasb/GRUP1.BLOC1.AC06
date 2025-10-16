a965a0717dc77d2deb04e98e08d7d72fe07955da
Cmorlasb <carlos.morlas.7e7@itb.cat>
2025-10-16 11:54:17 +0200

    Ex3

--- Act6/Program.cs
+++ Act6/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+using System;
+
+// PRECONDITION: The user is prompted to input the width and height of a rectangle.
+public class SumTwoNumbers
+{
+    public static void Main()
+    {
+        const string MSG_INPUT_WIDTH = "Enter the width of the rectangle: ";
+        const string MSG_INPUT_HEIGTH = "Enter the height of the rectangle: ";
+        const string MSG_RESULT = "The area of the rectangle is: ";
+        float width = 0;
+        float heigth = 0;
+        float area = 0;
+        Console.WriteLine(MSG_INPUT_WIDTH);
+        width = float.Parse(Console.ReadLine());
+        Console.WriteLine(MSG_INPUT_HEIGTH);
+        heigth = float.Parse(Console.ReadLine());
+        area = width * heigth;
+        Console.WriteLine(MSG_RESULT + area);
+    }
+}
+// POSTCONDITION: 'area' contains the product of 'width' and 'height', and it is displayed.
