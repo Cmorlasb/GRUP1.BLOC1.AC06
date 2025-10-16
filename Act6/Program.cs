6c726d9edd3b049aca95c67fa89869ad03ee7fdd
Cmorlasb <carlos.morlas.7e7@itb.cat>
2025-10-16 12:05:50 +0200

    Ex4

--- Act6/Program.cs
+++ Act6/Program.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.VisualBasic;
 using System;
 
-// PRECONDITION: The user is prompted to input a temperature in Celsius.
+// PRECONDITION: The user is prompted to input an amount in euros.
 public class Program
 {
     public static void Main()
     {
-        const string MSG_INPUT = "Enter termperature in Celsius: ";
-        const string MSG_RESULT = "Temperature in Kelvin: ";
-        float celsius = 0;
-        float kelvin = 0;
+        const float EXCHANGE_RATE = 1.12f;
+        const string MSG_INPUT = "Enter amount in euros: ";
+        const string MSG_RESULT = "Amount in dollars: ";
+        float euros = 0;
+        float dollars = 0;
         Console.WriteLine(MSG_INPUT);
-        celsius = float.Parse(Console.ReadLine());
-        kelvin = celsius + 273.15f;
-        Console.WriteLine(MSG_RESULT + kelvin);
+        euros = float.Parse(Console.ReadLine());
+        dollars = euros * EXCHANGE_RATE;
+        Console.WriteLine(MSG_RESULT + dollars);
     }
 }
-// POSTCONDITION: 'kelvin' contains the converted temperature, and it is displayed.
+// POSTCONDITION: 'dollars' contains the converted amount, and it is displayed.
